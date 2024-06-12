e4b24854f4eefd2b86a5ceabae179c4bee3966ce
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-12 13:00:02 +0300

    pa

---
+++ palindrome.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+	var word string
+	fmt.Println("** Palindrome checker ** ")
+	fmt.Print("Enter any word: ")
+
+	if os.Args != nil{
+
+		arg := os.Args[1]
+	}
+
+	fmt.Scan(&word)
+
+	mt.Println(word, )
+}
